454f87e7b4fb02c8f10f7214071d3c7663a50185
Crysta1221 <takuyakojim@outlook.jp>
2024-03-10 00:57:13 +0900

    :lipstick: Change OGP Layouts

--- components/OgImage/BlogPost.vue
+++ components/OgImage/BlogPost.vue
@@ -13,10 +13,10 @@ withDefaults(
   <div class="crysta_bg h-full w-full flex items-start justify-start">
     <div class="flex items-start justify-start h-full">
       <div class="flex flex-col justify-between w-full h-full">
-        <h1 class="ml-4 mt-8 text-[54px] font-bold p-24 text-left">
+        <h1 class="ml-8 mt-8 text-[50px] font-semibold p-24 text-left">
           {{ title }}
         </h1>
-        <div class="flex items-center pb-16 px-23 gap-3">
+        <div class="flex items-center pb-16 px-32 gap-3">
           <img class="size-[62px] rounded-full" src="/crysta.png" />
           <p class="text-3xl font-bold">CrystaWorld Blogs</p>
         </div>
